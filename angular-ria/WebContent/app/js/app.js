21186bb9d7adaa8739dd817d9906ad783ca5c6ee
Rob Barrett <rob.barrett@ventyx.abb.com>
2014-03-26 18:23:32 +1100

    added a barebones text input directive

--- angular-ria/WebContent/app/js/app.js
+++ angular-ria/WebContent/app/js/app.js
@@ -1,17 +1,10 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-var v = angular.module('angularjs-ria', [
+var v = angular.module('angular-ria', [
     'ngRoute',
-    'angularjs-ria-filters',
-    'angularjs-ria-services',
-    'angularjs-ria-directives',
-    'angularjs-ria-controllers'
+    'angular-ria-filters',
+    'angular-ria-services',
+    'angular-ria-directives',
+    'angular-ria-controllers'
 ]);
-
-v.config(['$routeProvider', function ($routeProvider) {
-    // TODO: remove this
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
--- angular-ria/WebContent/app/js/directives.js
+++ angular-ria/WebContent/app/js/directives.js
@@ -5,7 +5,19 @@
 var v = angular.module('angular-ria-directives', []);
 
 v.directive('appVersion', ['version', function (version) {
-    return function (scope, elm, attrs) {
-        elm.text(version);
+    return function (scope, element, attrs) {
+        element.text(version);
     };
 }]);
+
+v.directive('riaTextInput', [function () {
+    return {
+        restrict: 'E',
+        link: function (scope, elem, attrs) {
+            console.log("Recognized the riaTextInput");
+        },
+        template: '<input />'
+    };
+}])
+;
+
