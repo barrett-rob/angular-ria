4522de8f0cede76eb76dff2a92b89ae7e169904c
Rob Barrett <rob.barrett@ventyx.abb.com>
2014-04-10 19:41:26 +1000

    remove demo filter and tests

--- angular-ria/WebContent/app/js/filters.js
+++ angular-ria/WebContent/app/js/filters.js
@@ -3,9 +3,3 @@
 /* Filters */
 
 var v = angular.module('angular-ria-filters', []);
-
-v.filter('interpolate', ['version', function (version) {
-    return function (text) {
-        return String(text).replace(/\%VERSION\%/mg, version);
-    }
-}]);
